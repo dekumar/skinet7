ccf825a92b7f30cb4473a638a9f34a088b160b2e
Deepankar Kumar <dk.00kr@gmail.com>
2023-07-28 01:04:44 +0530

    End of section 3

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Core.Entities;
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -12,25 +13,46 @@ namespace API.Controllers {
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase {
 
-        private readonly StoreContext _context;
+        // here we are directly intracting with context
+        // private readonly StoreContext _context;
+        // public ProductsController(StoreContext context) {
+        //     _context = context;
+        // }
+        private readonly IProductRepository _repository;
 
-        public ProductsController(StoreContext context)
-        {
-            _context = context;
-            
+        //usign the repository to access database context
+        public ProductsController(IProductRepository repository) {
+            _repository = repository;
         }
+
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts() {
-            var products = await _context.Products.ToListAsync();
-            return products;
+            //var products = await _context.Products.ToListAsync();
+
+            //changing the method to access data via repository
+            var products = await _repository.GetProductsAsync();
+            return Ok(products);
             //return "this will return list of products";
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id) {
-            var product=await _context.Products.FindAsync(id);
+            //var product = await _context.Products.FindAsync(id);
+
+            //updating method to access the repository 
+            var product = await _repository.GetProductByIdAsync(id);
             return product;
             //return "this will be a product";
+        }
+
+        [HttpGet("brands")]
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands() {
+            return Ok(await _repository.GetProductBrandsAsync());
+        }
+
+        [HttpGet("types")]
+        public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes() {
+            return Ok(await _repository.GetProductTypesAsync());
         }
     }
 }
--- Core/Entities/Product.cs
+++ Core/Entities/Product.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Core.Entities
-{
-    public class Product
-    {
-        public int id { get; set; }
+namespace Core.Entities {
+    public class Product : BaseEntity {
         public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string PictureUrl { get; set; }
+        public ProductType ProductType { get; set; }
+        public int ProductTypeId { get; set; }
+        public ProductBrand ProductBrand { get; set; }
+        public int ProductBrandId { get; set; }
     }
 }
